585ef7371f863307be884bc35a3c0a61f3d49946
Finn-Oskar <finnoskar@mikkelsen.co.nz>
2026-06-11 15:15:52 +1200

    added products.ejs, product.ejs and css files + routes and prettyising

--- tayven/app.js
+++ tayven/app.js
@@ -16,8 +16,18 @@ app.get("/", (req, res) => {
     res.render("index");
     console.log('Home page loaded: /index')
 })
+app.get("/product/:name", (req, res) => {
+    res.render("product", {
+        name: req.params.name,
+        price: "$40",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus in officiis unde."
+    });
+})
+app.get("/products", (req, res) => {
+    res.render("products");
+})
 app.get("*", (req, res) => {
-    res.render("404");
+    res.render("404", { path: req.path});
 })
 app.listen(PORT, ()=>{console.log("Server running on port: " + PORT)});
 
