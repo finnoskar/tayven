245497720fdf64649c4e958f291ce5dfe41953c0
Spagta <102017291+finnoskar@users.noreply.github.com>
2026-07-02 14:18:10 +1200

    Revert "Footer 2 into main"

--- tayven/app.js
+++ tayven/app.js
@@ -13,38 +13,21 @@ app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        tayvenEmail: "tayven@example.com",
-        tayvenPhone: "022 something"
-    });
+    res.render("index");
     console.log('Home page loaded: /index')
 })
 app.get("/product/:name", (req, res) => {
     res.render("product", {
         name: req.params.name,
         price: "$40",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus in officiis unde.",
-        tayvenEmail: "tayven@example.com",
-        tayvenPhone: "022 something"
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus in officiis unde."
     });
 })
 app.get("/products", (req, res) => {
-    res.render("products", {
-        tayvenEmail: "tayven@example.com",
-        tayvenPhone: "022 something"
-    });
+    res.render("products");
 })
 app.get("/mowing", (req, res) => {
-    res.render("mowing", {
-        tayvenEmail: "tayven@example.com",
-        tayvenPhone: "022 something"
-    });
-})
-app.get("/cart", (req, res) => {
-    res.render("cart", {
-        tayvenEmail: "tayven@example.com",
-        tayvenPhone: "022 something"
-    });
+    res.render("mowing");
 })
 app.get("*", (req, res) => {
     res.render("404", { path: req.path});
