69a892b6be1e8418cf1405090dc7b5f919d76815
Spagta <102017291+finnoskar@users.noreply.github.com>
2026-09-01 14:55:51 +1200

    Update cart-routes.js
    added cart logic (might be wrong, could not test)
    integrated with session.cart
    added /order and /update logic.
    get("/") currently has placeholder quantities: quantities will need to be introduced to session.cart somehow?
    not sure how (will ahve to modify Product Model somehow)
    maybe create a front-end ProductOrder class which  includes a local quantity that can be added to a cart

--- tayven/routes/cart-routes.js
+++ tayven/routes/cart-routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import session from "express-session";
 
-import { tayvenInfo, db, products} from '../app.js'
+import { tayvenInfo, db, products} from '../app.js';
+import { ProductModel } from '../data.js';
 
 const cartRouter = express.Router();
 
@@ -9,15 +10,36 @@ cartRouter.get("/", (req, res) => {
     if (req.session.cart) {
         res.render("cart", { 
             products: req.session.cart,
+            quantities: req.session.cart.map(item => {1}),
             ...tayvenInfo
         });
     }
 })
 cartRouter.post("/order", (req, res) => {
-    res.send("ordered");
+    const order = [];
+    for (const [sku, {name, quantity}] of Object.entries(req.body.items)) {
+        console.log(`Ordered ${quantity} of product "${name}", (sku ${sku})`);
+    }
+    // use email api to send tayven an email about the thing, possibly use an orders database
+    
 })
 cartRouter.post("/update", (req, res) => {
-    res.send("updated");
+    const { items, contact, action } = req.body;
+    let newProducts = [];
+    let quantities = [];
+    for (const sku in items) {
+        const p = ProductModel.findOne({ sku: sku });
+        if (p) {
+            newProducts.push(p);
+            quantities.push(Number(items[sku].quantity));
+        }
+    }
+    req.session.cart = newProducts;
+    res.render("cart", {
+        products: newProducts.
+        quantities: quantities,
+        ...tayvenInfo
+    });
 })
 
 export default cartRouter;
