ecd6bf1e7999ed7148d4a4c798f9e2c4921ef774
Spagta <102017291+finnoskar@users.noreply.github.com>
2026-09-03 20:54:22 +1200

    Update cart-routes.js
    fixed async/await issues (i forgot the keywords)
    also updated the code to the cart = [{...product, quantity}] form so that the /update route works as intended

--- tayven/routes/cart-routes.js
+++ tayven/routes/cart-routes.js
@@ -6,15 +6,6 @@ import { tayvenInfo, db, products} from '../app.js';
 import { ProductModel } from '../data.js';
 
 const cartRouter = express.Router();
-cartRouter.use(session({
-    secret: process.env.SESSION_SECRET || "1234567890f385dbe378twlgrfy",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60, // after development, set it to 1000 * 60 * 60 * 2
-        secure: false,
-    }
-}));
 
 cartRouter.get("/", (req, res) => {
     console.log("cart page");
@@ -40,20 +31,23 @@ cartRouter.get("/", (req, res) => {
 cartRouter.post("/order", (req, res) => {
     const order = [];
     for (const [sku, {name, quantity}] of Object.entries(req.body.items)) {
-        console.log(`Ordered ${quantity} of product "${name}", (sku ${sku})`);
+        console.log(`Ordered ${quantity} of sku ${sku}.`);
     }
     // use email api to send tayven an email about the thing, possibly use an orders database
     
 })
-cartRouter.post("/update", (req, res) => {
+cartRouter.post("/update", async (req, res) => {
     const { items, contact, action } = req.body;
     let newProducts = [];
-    let quantities = [];
     for (const sku in items) {
-        const p = ProductModel.findOne({ sku: sku });
+        const p = await ProductModel.findOne({ sku: sku });
         if (p) {
-            newProducts.push(p);
-            quantities.push(Number(items[sku].quantity));
+            newProducts.push({
+                sku: p.sku,
+                name: p.name,
+                price: p.price,
+                quantity: Number(items[sku].quantity)
+            });
         }
     }
     req.session.cart = newProducts;
